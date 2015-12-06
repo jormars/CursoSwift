2d773cbe767196ec56ed0575470e3f9ee477ac4b
Jorge Martín <jormars2@gmail.com>
2015-12-06 20:34:05 +0100

    Initial Commit

---
+++ Memoria 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memoria 2
+//
+//  Created by Jorge Martín on 6/12/15.
+//  Copyright © 2015 Jorge Martín. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
